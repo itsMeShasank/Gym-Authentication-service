18d7dd302d87cf9f07aaa2c38016c3d0a2f1e0fc
shasank gadipilli <gadipillishasank@gmail.com>
2023-08-19 20:47:45 +0530

    initial commit

---
+++ src/test/java/com/epam/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.epam.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@EnableAutoConfiguration(exclude = {KafkaAutoConfiguration.class, DataSourceAutoConfiguration.class})
+class AuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
